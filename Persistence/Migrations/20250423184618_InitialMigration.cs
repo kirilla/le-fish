2ff3764be3d4c31ec47b7bf3d3a2568040526215
Jonas Sundström <jonas@kirilla.se>
2025-04-23 20:49:43 +0200

    First!

---
+++ Persistence/Migrations/20250423184618_InitialMigration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lefish.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Removed, to -IgnoreChanges
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Removed, to -IgnoreChanges
+        }
+    }
+}
