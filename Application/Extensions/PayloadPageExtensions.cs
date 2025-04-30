4bc699da16973e97166e2063d62731d3d69bad29
Jonas Sundström <jonas@kirilla.se>
2025-04-30 12:18:01 +0200

    Substitute!

--- Application/Extensions/PayloadPageExtensions.cs
+++ Application/Extensions/PayloadPageExtensions.cs
@@ -4,6 +4,8 @@ public static class PayloadPageExtensions
 {
     public static void InsertTargetValues(this PayloadPage page, EmailTarget target)
     {
+        page.Html = page.Html.Replace("[[target_name]]", target.Name);
+        page.Html = page.Html.Replace("[[target_address]]", target.Address);
         page.Html = page.Html.Replace("[[target_key]]", target.PersonKey);
     }
 
