124679a90d17b0dfaa03050833a0b42008c64d7c
Jonas Sundström <jonas@kirilla.se>
2025-04-30 19:40:31 +0200

    Add a page to show available targets for a page.

---
+++ Web/Pages/PayloadPages/ShowPayloadPageTargets.cshtml.cs
@@ -0,0 +1,38 @@
+﻿namespace Lefish.Web.Pages.PayloadPages;
+
+public class ShowPayloadPageTargetsModel(
+    IUserToken userToken,
+    IDatabaseService database) : UserTokenPageModel(userToken)
+{
+    public PayloadPage PayloadPage { get; set; }
+
+    public List<EmailTarget> EmailTargets { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int id)
+    {
+        try
+        {
+            if (!UserToken.IsAuthenticated)
+                throw new NotPermittedException();
+
+            PayloadPage = await database.PayloadPages
+                .Where(x => x.Id == id)
+                .SingleOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            EmailTargets = await database.EmailTargets
+                .OrderBy(x => x.Address)
+                .ToListAsync();
+
+            return Page();
+        }
+        catch (NotFoundException)
+        {
+            return Redirect("/help/notfound");
+        }
+        catch
+        {
+            return Redirect("/help/notpermitted");
+        }
+    }
+}
