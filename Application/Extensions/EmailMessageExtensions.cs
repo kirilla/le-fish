dd64279521a7c9caf6ecf94b32fc697757c614ac
Jonas Sundström <jonas@kirilla.se>
2025-04-30 11:03:38 +0200

    Also replace the target_key value.

--- Application/Extensions/EmailMessageExtensions.cs
+++ Application/Extensions/EmailMessageExtensions.cs
@@ -6,11 +6,14 @@ public static class EmailMessageExtensions
     {
         message.Subject = message.Subject.Replace("[[target_name]]", target.Name);
         message.Subject = message.Subject.Replace("[[target_address]]", target.Address);
+        message.Subject = message.Subject.Replace("[[target_key]]", target.PersonKey?.ToString() ?? "null");
 
         message.HtmlBody = message.HtmlBody.Replace("[[target_name]]", target.Name);
         message.HtmlBody = message.HtmlBody.Replace("[[target_address]]", target.Address);
+        message.HtmlBody = message.HtmlBody.Replace("[[target_key]]", target.PersonKey?.ToString() ?? "null");
 
         message.TextBody = message.TextBody.Replace("[[target_name]]", target.Name);
         message.TextBody = message.TextBody.Replace("[[target_address]]", target.Address);
+        message.TextBody = message.TextBody.Replace("[[target_key]]", target.PersonKey?.ToString() ?? "null");
     }
 }
