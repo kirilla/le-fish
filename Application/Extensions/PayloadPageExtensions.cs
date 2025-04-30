9e7155e54eee11df43ba534c71f966fc4e4ed8be
Jonas Sundström <jonas@kirilla.se>
2025-04-30 10:50:48 +0200

    id => key

--- Application/Extensions/PayloadPageExtensions.cs
+++ Application/Extensions/PayloadPageExtensions.cs
@@ -4,6 +4,6 @@ public static class PayloadPageExtensions
 {
     public static void InsertTargetValues(this PayloadPage page, EmailTarget target)
     {
-        page.Html = page.Html.Replace("[[target:id]]", target.PersonKey?.ToString());
+        page.Html = page.Html.Replace("[[target:key]]", target.PersonKey?.ToString());
     }
 }
