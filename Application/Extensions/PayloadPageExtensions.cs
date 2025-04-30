cbcdba082d864939022bfa062ac5e71180f7f473
Jonas Sundström <jonas@kirilla.se>
2025-04-30 12:17:27 +0200

    We're all strings now.

--- Application/Extensions/PayloadPageExtensions.cs
+++ Application/Extensions/PayloadPageExtensions.cs
@@ -4,7 +4,7 @@ public static class PayloadPageExtensions
 {
     public static void InsertTargetValues(this PayloadPage page, EmailTarget target)
     {
-        page.Html = page.Html.Replace("[[target_key]]", target.PersonKey?.ToString());
+        page.Html = page.Html.Replace("[[target_key]]", target.PersonKey);
     }
 
     public static void GeneratePhishingKey(this PayloadPage page, List<string> takenKeys)
