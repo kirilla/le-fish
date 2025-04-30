079195fab518826e8d270a2b5723aa810b79b02a
Jonas Sundström <jonas@kirilla.se>
2025-04-30 11:02:41 +0200

    Switch from colon separator to underscore.
    Perhaps this will lessen confusion, and risk of interpreting the variable as key:value notation.

--- Application/Extensions/EmailMessageExtensions.cs
+++ Application/Extensions/EmailMessageExtensions.cs
@@ -4,13 +4,13 @@ public static class EmailMessageExtensions
 {
     public static void InsertTargetValues(this EmailMessage message, EmailTarget target)
     {
-        message.Subject = message.Subject.Replace("[[target:name]]", target.Name);
-        message.Subject = message.Subject.Replace("[[target:address]]", target.Address);
+        message.Subject = message.Subject.Replace("[[target_name]]", target.Name);
+        message.Subject = message.Subject.Replace("[[target_address]]", target.Address);
 
-        message.HtmlBody = message.HtmlBody.Replace("[[target:name]]", target.Name);
-        message.HtmlBody = message.HtmlBody.Replace("[[target:address]]", target.Address);
+        message.HtmlBody = message.HtmlBody.Replace("[[target_name]]", target.Name);
+        message.HtmlBody = message.HtmlBody.Replace("[[target_address]]", target.Address);
 
-        message.TextBody = message.TextBody.Replace("[[target:name]]", target.Name);
-        message.TextBody = message.TextBody.Replace("[[target:address]]", target.Address);
+        message.TextBody = message.TextBody.Replace("[[target_name]]", target.Name);
+        message.TextBody = message.TextBody.Replace("[[target_address]]", target.Address);
     }
 }
