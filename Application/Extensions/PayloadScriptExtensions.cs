bab870bc81029604c397e8a0732820c78b355781
Jonas Sundström <jonas@kirilla.se>
2025-05-20 19:03:04 +0200

    Add a payload script delivery page.

---
+++ Application/Extensions/PayloadScriptExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Lefish.Common.Settings;
+
+namespace Lefish.Application.Extensions;
+
+public static class PayloadScriptExtensions
+{
+    public static void InsertTargetValues(
+        this PayloadScript script,
+        TemplateConfiguration templateConfiguration,
+        EmailTarget target, 
+        PageToken token)
+    {
+        script.Script = script.Script.Replace(Variables.PAGE_URL, templateConfiguration.PageUrl);
+        script.Script = script.Script.Replace(Variables.TARGET_NAME, target.Name);
+        script.Script = script.Script.Replace(Variables.TARGET_ADDRESS, target.Address);
+        script.Script = script.Script.Replace(Variables.PAGE_TOKEN, token.Token.ToString());
+    }
+}
---
+++ Web/Pages/Script.cshtml.cs
@@ -0,0 +1,78 @@
+using Lefish.Application.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace Lefish.Web.Pages;
+
+[IgnoreAntiforgeryToken]
+[AllowAnonymous]
+public class ScriptPageModel(
+    IUserToken userToken,
+    IDatabaseService database,
+    IOptions<TemplateConfiguration> templateConfiguration) : UserTokenPageModel(userToken)
+{
+    private readonly TemplateConfiguration _config = templateConfiguration.Value;
+
+    public EmailTarget EmailTarget { get; set; }
+    public PageToken PageToken { get; set; }
+    public PayloadScript PayloadScript { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int token)
+    {
+        try
+        {
+            PageToken = await database.PageTokens
+                .AsNoTracking()
+                .Where(x => x.Token == token)
+                .SingleOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            PayloadScript = await database.PayloadScripts
+                .AsNoTracking()
+                .Where(x => x.Id == PageToken.PayloadPageId)
+                .SingleOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            var emailTargetId = await database.PageTokens
+                .AsNoTracking()
+                .Where(x => x.Token == token)
+                .Select(x => x.EmailMessage.EmailTargetId)
+                .Cast<int?>()
+                .SingleOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            EmailTarget = await database.EmailTargets
+                .AsNoTracking()
+                .Where(x => x.Id == emailTargetId)
+                .SingleOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            PayloadScript.InsertTargetValues(_config, EmailTarget, PageToken);
+
+            await LogPageVisit(HttpContext, PageToken);
+
+            return Page();
+        }
+        catch
+        {
+            return NotFound();
+        }
+    }
+
+    public async Task LogPageVisit(
+        HttpContext context, PageToken pageToken)
+    {
+        var visit = new PageVisit()
+        {
+            Url = context.Request.GetDisplayUrl(),
+            Method = context.Request.Method,
+            IpAddress = context.Connection.RemoteIpAddress?.ToString(),
+            UserAgent = context.Request.Headers?.UserAgent,
+            PageTokenId = pageToken.Id,
+        };
+
+        database.PageVisits.Add(visit);
+
+        await database.SaveAsync(UserToken);
+    }
+}
