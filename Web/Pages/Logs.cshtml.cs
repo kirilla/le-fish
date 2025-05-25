f5be191539e0b2f25f6881fb23bb2a9d7b0f4ffa
Jonas Sundström <jonas@kirilla.se>
2025-05-25 22:24:31 +0200

    Add a logs page.

---
+++ Web/Pages/Logs.cshtml.cs
@@ -0,0 +1,25 @@
+﻿namespace Lefish.Web.Pages;
+
+public class LogsModel(
+    IUserToken userToken,
+    IDatabaseService database) : UserTokenPageModel(userToken)
+{
+    public IActionResult OnGet()
+    {
+        try
+        {
+            if (!UserToken.IsAuthenticated)
+                throw new NotPermittedException();
+
+            return Page();
+        }
+        catch (NotFoundException)
+        {
+            return Redirect("/help/notfound");
+        }
+        catch
+        {
+            return Redirect("/help/notpermitted");
+        }
+    }
+}
