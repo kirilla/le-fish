4af24e91c62930a8afc645619373514e790fb44a
Jonas Sundström <jonas@kirilla.se>
2025-06-03 20:57:59 +0200

    Add instruction fetching.

---
+++ Application/Extensions/TargetInstructionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Lefish.Common.Settings;
+
+namespace Lefish.Application.Extensions;
+
+public static class TargetInstructionExtensions
+{
+    public static string ReplaceVariables(
+        this TargetInstruction instruction,
+        TemplateConfiguration templateConfiguration,
+        EmailTarget target, 
+        Attack attack)
+    {
+        string s = instruction.Script;
+
+        // Step 1
+        s = s.Replace(Variables.PAGE_URL, templateConfiguration.PageUrl);
+        s = s.Replace(Variables.SCRIPT_URL, templateConfiguration.ScriptUrl);
+
+        // Step 2
+        s = s.Replace(Variables.ATTACK_TOKEN, attack.Value.ToString());
+        s = s.Replace(Variables.TARGET_ADDRESS, target.Address);
+        s = s.Replace(Variables.TARGET_NAME, target.Name);
+
+        // Q: Why two steps?
+        // A: PAGE_URL and SCRIPT_URL may contain PAGE_TOKEN.
+
+        return s;
+    }
+}
---
+++ Web/Pages/FetchInstruction.cshtml.cs
@@ -0,0 +1,56 @@
+using Lefish.Application.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using System.Text;
+
+namespace Lefish.Web.Pages;
+
+[IgnoreAntiforgeryToken]
+[AllowAnonymous]
+public class FetchInstructionModel(
+    IUserToken userToken,
+    IDatabaseService database,
+    IOptions<TemplateConfiguration> templateConfiguration) : UserTokenPageModel(userToken)
+{
+    private readonly TemplateConfiguration _config = templateConfiguration.Value;
+
+    public async Task<IActionResult> OnGetAsync(int token)
+    {
+        try
+        {
+            var attack = await database.Attacks
+                .AsNoTracking()
+                .Where(x => x.Value == token)
+                .SingleOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            var target = await database.EmailTargets
+                .AsNoTracking()
+                .Where(x => x.Id == attack.EmailTargetId)
+                .SingleOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            var instruction = await database.TargetInstructions
+                .Where(x => 
+                    x.AttackId == attack.Id && 
+                    x.InstructionStatus == InstructionStatus.Available)
+                .OrderBy(x => x.Created)
+                .FirstOrDefaultAsync() ??
+                throw new NotFoundException();
+
+            if (instruction == null)
+                return NotFound();
+
+            instruction.InstructionStatus = InstructionStatus.Fetched;
+
+            await database.SaveAsync(UserToken);
+
+            var script = instruction.ReplaceVariables(_config, target, attack);
+
+            return Content(script, "application/javascript", Encoding.UTF8);
+        }
+        catch
+        {
+            return NotFound();
+        }
+    }
+}
