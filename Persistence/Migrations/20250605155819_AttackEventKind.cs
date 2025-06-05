4ab3954eafe5838705a6a06f2bea10a537bd93a0
Jonas Sundström <jonas@kirilla.se>
2025-06-05 18:05:06 +0200

    Enum db migration.

---
+++ Persistence/Migrations/20250605155819_AttackEventKind.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lefish.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AttackEventKind : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VisitKind",
+                table: "AttackEvents",
+                newName: "AttackEventKind");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AttackEventKind",
+                table: "AttackEvents",
+                newName: "VisitKind");
+        }
+    }
+}
