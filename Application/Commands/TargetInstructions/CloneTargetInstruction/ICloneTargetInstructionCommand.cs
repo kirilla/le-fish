e88bf9ed2fb5b5747c200a85bf93cb9e7c9cc8ce
Jonas Sundström <jonas@kirilla.se>
2025-06-08 18:18:09 +0200

    Drop the CloneTargetInstruction operation.

--- Application/Commands/TargetInstructions/CloneTargetInstruction/ICloneTargetInstructionCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lefish.Application.Commands.TargetInstructions.CloneTargetInstruction;
-
-public interface ICloneTargetInstructionCommand
-{
-    Task<int> Execute(IUserToken userToken, CloneTargetInstructionCommandModel model);
-
-    bool IsPermitted(IUserToken userToken);
-}
