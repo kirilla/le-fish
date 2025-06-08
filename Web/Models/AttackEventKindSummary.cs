994d7ead26bc138ba4a991f5d697802e513190b0
Jonas Sundström <jonas@kirilla.se>
2025-06-08 22:36:59 +0200

    Add missing.

---
+++ Web/Models/AttackEventKindSummary.cs
@@ -0,0 +1,15 @@
+﻿namespace Lefish.Web.Models;
+
+public class AttackEventKindSummary
+{
+    public int PageVisitCount { get; set; }
+    public int ScriptDownloadCount { get; set; }
+    public int FetchInstructionCount { get; set; }
+    public int UploadDataCount { get; set; }
+
+    public int Total =>
+        PageVisitCount +
+        ScriptDownloadCount +
+        FetchInstructionCount +
+        UploadDataCount;
+}
